562ac1c0f5ec3362ee00051c47e704dfbf380e8a
coder_wen <qtm4081@dingtalk.com>
2022-07-05 17:26:06 +0800

    add: 适配器增加批量支持

--- src/adapters/DatabaseAdapter.php
+++ src/adapters/DatabaseAdapter.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace teamones\casbin\adapters;
 
-use Casbin\Persist\Adapter as AdapterContract;
+use think\facade\Db;
 use Casbin\Model\Model;
+use Casbin\Persist\Adapter;
+use Casbin\Persist\BatchAdapter;
 use Casbin\Persist\AdapterHelper;
+use think\db\exception\DbException;
+use Casbin\Persist\UpdatableAdapter;
+use think\db\exception\DataNotFoundException;
+use think\db\exception\ModelNotFoundException;
 
 /**
  * DatabaseAdapter.
  */
-class DatabaseAdapter implements AdapterContract
+class DatabaseAdapter implements Adapter, UpdatableAdapter, BatchAdapter
 {
 
     use AdapterHelper;
@@ -19,7 +25,7 @@ class DatabaseAdapter implements AdapterContract
     /**
      * @var \think\Model
      */
-    protected $eloquent;
+    protected \think\Model $eloquent;
 
     /**
      * DatabaseAdapter constructor.
@@ -49,9 +55,9 @@ class DatabaseAdapter implements AdapterContract
     /**
      * loads all policy rules from the storage.
      * @param Model $model
-     * @throws \think\db\exception\DataNotFoundException
-     * @throws \think\db\exception\DbException
-     * @throws \think\db\exception\ModelNotFoundException
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws ModelNotFoundException
      */
     public function loadPolicy(Model $model): void
     {
@@ -105,9 +111,9 @@ class DatabaseAdapter implements AdapterContract
      * @param string $sec
      * @param string $ptype
      * @param array $rule
-     * @throws \think\db\exception\DataNotFoundException
-     * @throws \think\db\exception\DbException
-     * @throws \think\db\exception\ModelNotFoundException
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws ModelNotFoundException
      */
     public function removePolicy(string $sec, string $ptype, array $rule): void
     {
@@ -132,9 +138,9 @@ class DatabaseAdapter implements AdapterContract
      * @param string $ptype
      * @param int $fieldIndex
      * @param string ...$fieldValues
-     * @throws \think\db\exception\DataNotFoundException
-     * @throws \think\db\exception\DbException
-     * @throws \think\db\exception\ModelNotFoundException
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws ModelNotFoundException
      */
     public function removeFilteredPolicy(string $sec, string $ptype, int $fieldIndex, string ...$fieldValues): void
     {
@@ -154,4 +160,166 @@ class DatabaseAdapter implements AdapterContract
         }
     }
 
+    /**
+     * Filter the rule.
+     *
+     * @param array $rule
+     * @return array
+     */
+    public function filterRule(array $rule): array
+    {
+        $rule = array_values($rule);
+
+        $i = count($rule) - 1;
+        for (; $i >= 0; $i--) {
+            if ($rule[$i] != '' && !is_null($rule[$i])) {
+                break;
+            }
+        }
+
+        return array_slice($rule, 0, $i + 1);
+    }
+
+    /**
+     * 移除策略
+     * @param string $sec
+     * @param string $ptype
+     * @param int $fieldIndex
+     * @param string|null ...$fieldValues
+     * @return array
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws ModelNotFoundException
+     */
+    public function _removeFilteredPolicy(string $sec, string $ptype, int $fieldIndex, ?string ...$fieldValues): array
+    {
+        $count = 0;
+        $removedRules = [];
+
+        $instance = $this->eloquent->where('ptype', $ptype);
+        foreach (range(0, 5) as $value) {
+            if ($fieldIndex <= $value && $value < $fieldIndex + count($fieldValues)) {
+                if ('' != $fieldValues[$value - $fieldIndex]) {
+                    $instance->where('v' . strval($value), $fieldValues[$value - $fieldIndex]);
+                }
+            }
+        }
+
+        foreach ($instance->select() as $model) {
+            $item = $model->hidden(['id', 'ptype'])->toArray();
+            $item = $this->filterRule($item);
+            $removedRules[] = $item;
+            if ($model->delete()) {
+                ++$count;
+            }
+        }
+
+        return $removedRules;
+    }
+
+    /**
+     * 批量添加策略
+     * Adds a policy rules to the storage.
+     * This is part of the Auto-Save feature.
+     *
+     * @param string $sec
+     * @param string $ptype
+     * @param string[][] $rules
+     */
+    public function addPolicies(string $sec, string $ptype, array $rules): void
+    {
+        $cols = [];
+        $i = 0;
+
+        foreach ($rules as $rule) {
+            $temp['ptype'] = $ptype;
+            foreach ($rule as $key => $value) {
+                $temp['v' . strval($key)] = $value;
+            }
+            $cols[$i++] = $temp;
+            $temp = [];
+        }
+        $this->eloquent->insertAll($cols);
+    }
+
+
+    /**
+     * 批量更新策略
+     * @param string $sec
+     * @param string $ptype
+     * @param array $oldRules
+     * @param array $newRules
+     * @return void
+     */
+    public function updatePolicies(string $sec, string $ptype, array $oldRules, array $newRules): void
+    {
+        Db::transaction(function () use ($sec, $ptype, $oldRules, $newRules) {
+            foreach ($oldRules as $i => $oldRule) {
+                $this->updatePolicy($sec, $ptype, $oldRule, $newRules[$i]);
+            }
+        });
+    }
+
+    /**
+     * 批量更新策略
+     * @param string $sec
+     * @param string $ptype
+     * @param array $newPolicies
+     * @param int $fieldIndex
+     * @param string ...$fieldValues
+     * @return array
+     */
+    public function updateFilteredPolicies(string $sec, string $ptype, array $newPolicies, int $fieldIndex, string ...$fieldValues): array
+    {
+        $oldRules = [];
+        DB::transaction(function () use ($sec, $ptype, $fieldIndex, $fieldValues, $newPolicies, &$oldRules) {
+            $oldRules = $this->_removeFilteredPolicy($sec, $ptype, $fieldIndex, ...$fieldValues);
+            $this->addPolicies($sec, $ptype, $newPolicies);
+        });
+
+        return $oldRules;
+    }
+
+    /**
+     * 批量移除策略
+     * @param string $sec
+     * @param string $ptype
+     * @param array $rules
+     * @return void
+     */
+    public function removePolicies(string $sec, string $ptype, array $rules): void
+    {
+        Db::transaction(function () use ($sec, $ptype, $rules) {
+            foreach ($rules as $rule) {
+                $this->removePolicy($sec, $ptype, $rule);
+            }
+        });
+    }
+
+    /**
+     * 更新策略
+     * @param string $sec
+     * @param string $ptype
+     * @param array $oldRule
+     * @param array $newPolicy
+     * @return void
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws ModelNotFoundException
+     */
+    public function updatePolicy(string $sec, string $ptype, array $oldRule, array $newPolicy): void
+    {
+        $instance = $this->eloquent->where('ptype', $ptype);
+        foreach ($oldRule as $key => $value) {
+            $instance->where('v' . strval($key), $value);
+        }
+        $instance = $instance->find();
+
+        foreach ($newPolicy as $key => $value) {
+            $column = 'v' . strval($key);
+            $instance->$column = $value;
+        }
+
+        $instance->save();
+    }
 }
